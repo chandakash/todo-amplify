e91956d6655f0aa4e075d5cfedde73afe16f54e0
Akash Chand <akash@bombayplay.com>
2023-06-09 10:02:08 +0530

    update: changes after adding api

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,6 +1,6 @@
 export type AmplifyDependentResourcesAttributes = {
   "api": {
-    "todoapp": {
+    "todoamplify": {
       "GraphQLAPIEndpointOutput": "string",
       "GraphQLAPIIdOutput": "string",
       "GraphQLAPIKeyOutput": "string"
